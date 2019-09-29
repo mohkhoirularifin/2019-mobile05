25da56e9cbb0a6f0cbba66b2ba596529b4a0745d
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-09-30 00:57:52 +0700

    Tugas Fragment - Dips

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/FitnesKu.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/FitnesKu.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.DipsFragment;
 import polinema.ac.id.starterchapter05.fragments.FitnessFragment;
 
 public class FitnesKu extends AppCompatActivity {
@@ -37,5 +38,17 @@ public class FitnesKu extends AppCompatActivity {
     }
 
     public void handlerDips(View view) {
+        DipsFragment checkVisible = (DipsFragment) getSupportFragmentManager().findFragmentByTag("FRAGMENT_DIPS");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if (checkVisible != null && checkVisible.isVisible() ){
+//            fragmentTransaction.commit();
+        }
+        else {
+//            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.myFitnessPlaceholder,new DipsFragment(),"FRAGMENT_DIPS");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 }
