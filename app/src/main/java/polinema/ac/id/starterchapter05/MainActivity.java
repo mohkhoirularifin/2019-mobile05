b11a5a16cc485bbfd19cb4a2c710f93fa2f02980
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-09-30 00:27:41 +0700

    Tugas Fragment - Handstand

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import polinema.ac.id.starterchapter05.activities.DynamicActivity;
+import polinema.ac.id.starterchapter05.activities.FitnesKu;
 import polinema.ac.id.starterchapter05.activities.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,5 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handlerPraktikumFragment(View view) {
+        Intent i = new Intent(this, FitnesKu.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/polinema/ac/id/starterchapter05/activities/FitnesKu.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/FitnesKu.java
@@ -1,10 +1,13 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.FitnessFragment;
 
 public class FitnesKu extends AppCompatActivity {
 
@@ -12,5 +15,27 @@ public class FitnesKu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitnes_ku);
+    }
+
+    public void handlerPushUp(View view) {
+    }
+
+    public void handlerHandStand(View view) {
+
+        FitnessFragment checkVisible = (FitnessFragment) getSupportFragmentManager().findFragmentByTag("FRAGMENT_FITNESS");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if (checkVisible != null && checkVisible.isVisible() ){
+//            fragmentTransaction.commit();
+        }
+        else {
+//            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.myFitnessPlaceholder,new FitnessFragment(),"FRAGMENT_FITNESS");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void handlerDips(View view) {
     }
 }
